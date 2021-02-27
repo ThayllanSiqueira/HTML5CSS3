ba19ff834ebb8dd29bf15c2b5f35b7d82efce323
ThayllanSiqueira <thayllansiqueira@gmail.com>
2021-02-27 20:54:17 -0300

    Cursos de HTML5 Feitos

---
+++ 1_2_HTML_JAVASCRIPT_PHP/capitulo_10_jquery/mirror-fashion/js/produto.js
@@ -0,0 +1,5 @@
+function mostraTamanho() {
+    var inputTamanho = document.querySelector('[name=tamanho]')
+    var outputTamanho = document.querySelector('[name=valortamanho]')
+    outputTamanho.value = inputTamanho.value;
+}
